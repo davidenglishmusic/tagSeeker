24bd71d1b4068740bf2213c2055e318524a5e19d
David English <david@operaonfilm.com>
2015-02-12 22:01:27 -0800

    Determined which line to read to get tag information from a flac file

--- app.rb
+++ app.rb
@@ -1,17 +1,18 @@
-mp3 = File.new('AMbient.mp3')
+# mp3 = File.new('AMbient.mp3')
+#
+# mp3.seek(-128, IO::SEEK_END)
+#
+# p mp3.readline
+#
 
-mp3.seek(-128, IO::SEEK_END)
+#m4a = IO.readlines('AMbient.m4a')[1]
 
-p mp3.readline
+# m4a = File.new('AMbient.m4a')
+#
+# m4a.seek(-128, IO::SEEK_END)
+#
+# p m4a.readline
+#
+flac = IO.readlines('AMbient.flac')[1]
 
-m4a = File.new('AMbient.m4a')
-
-m4a.seek(-128, IO::SEEK_END)
-
-p m4a.readline
-
-flac = File.new('AMbient.flac')
-
-flac.seek(-128, IO::SEEK_END)
-
-p flac.readline
+p flac
