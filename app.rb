0d9a1f4fcf09ab1283bf9db4b7f99bfe3a964c85
David English <david@operaonfilm.com>
2015-02-14 19:47:30 -0800

    Determined meta location for m4a files

--- app.rb
+++ app.rb
@@ -1,18 +1,5 @@
-# mp3 = File.new('AMbient.mp3')
-#
-# mp3.seek(-128, IO::SEEK_END)
-#
-# p mp3.readline
-#
+puts IO.readlines('AMbient.mp3')[-1]
 
-#m4a = IO.readlines('AMbient.m4a')[1]
+puts IO.readlines('AMbient.m4a')[-1]
 
-# m4a = File.new('AMbient.m4a')
-#
-# m4a.seek(-128, IO::SEEK_END)
-#
-# p m4a.readline
-#
-flac = IO.readlines('AMbient.flac')[1]
-
-p flac
+puts IO.readlines('AMbient.flac')[1]
